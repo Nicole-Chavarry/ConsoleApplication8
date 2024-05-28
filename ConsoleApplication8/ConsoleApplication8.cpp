e2180e94dcdc1bf0b3502fc4a2995a2c713303f1
Nicole-Chavarry <145869229+Nicole-Chavarry@users.noreply.github.com>
2024-05-28 15:43:07 +0300

    ConsoleApplication8.cpp

--- ConsoleApplication8/ConsoleApplication8.cpp
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -5,7 +5,7 @@
 
 
 int main() {
-    // 1. Организуйте чтение из файла целочисленного одномерного вектора
+    // 1.  Organizar la lectura de un archivo de vector unidimensional entero
     std::vector<int> v;
     std::ifstream file("Texto.txt");
     if (!file.is_open()) {
@@ -18,23 +18,23 @@ int main() {
     }
     file.close();
 
-    // 2. Реверсируйте последовательность элементов вектора
+    // 2. Invertir la secuencia de elementos del vector
     reverse(v.begin(), v.end());
 
-    // 3. Найдите минимальный элемент
+    // 3. Encuentra el elemento mínimo
     auto min_element = *std::min_element(v.begin(), v.end());
 
-    // 4. Удалите из вектора все четные элементы
+    // 4. Elimine todos los elementos pares del vector
     v.erase(remove_if(v.begin(), v.end(), [](int x) { return x % 2 == 0; }), v.end());
 
-    // 5. Отсортируйте вектор в убывающей или возрастающей последовательности
+    // 5. Ordene el vector en secuencia descendente o ascendente
     sort(v.begin(), v.end(), std::greater<int>()); // убывающая последовательность
     // sort(v.begin(), v.end()); // возрастающая последовательность
 
-    // 6. Вставьте в вектор произвольный элемент, не нарушая сортировку
+    // 6. Inserte un elemento arbitrario en el vector sin romper la clasificación
     v.insert(upper_bound(v.begin(), v.end(), 5), 5); // вставить 5 в отсортированный вектор
 
-    // 7. Определите индекс заданного элемента
+    // 7.  Defina el índice del elemento especificado
     auto it = find(v.begin(), v.end(), 5);
     if (it != v.end()) {
         std::cout << "Índice del elemento:" << distance(v.begin(), it) << std::endl;
@@ -43,11 +43,12 @@ int main() {
         std::cout << "Elemento no encontrado" << std::endl;
     }
 
-    // 8. Удалите из вектора дублированные элементы.
+    // 8. Elimine los elementos duplicados del vector..
     sort(v.begin(), v.end()); // сортируем вектор
     v.erase(unique(v.begin(), v.end()), v.end()); // удаляем дубликаты
 
-    // Выводим результат
+    // Salida del resultado
+
     for (int x : v) {
         std::cout << x << " ";
     }
